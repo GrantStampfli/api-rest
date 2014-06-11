2d70cc980c575fb343c8a8a999b773b3fe4f1050
Tian Song <tian.y.song@gmail.com>
2014-06-11 11:30:55 -0700

    fixed signature issue

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-var url = 'https://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=4cd56a0ac053497268c94cc90da8b9a5&format=json&api_sig=31fde54e12236c5cd3b48ac28e1be278';
+var url = 'https://api.flickr.com/services/rest/?' + 
+'method=flickr.interestingness.getList&' + 
+'api_key=72ba71f6b1a5511c33d79e03be3ff7b7&' + 
+'format=json';
 $.ajax(url, { dataType: 'jsonp', jsonp: 'jsoncallback' })
 	.then(function(data, status, xhr) {
 		console.log(status);
