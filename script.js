c2e882eb211e96ada47a032415ee98e448e4723e
Ariel Spear <simpleredfox@gmail.com>
2014-06-11 13:59:24 -0700

    Real URLS, hurray.

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ function generateUrl(arrayOfObj) {
 	arrayOfObj.forEach(function(picObj){
 		var picUrl = 'https://farm' + picObj.farm +
 		'.staticflickr.com/' + picObj.server + '/' +
-		picObj.photo.id + '_'+ picObj.secret +'.jpg';
+		picObj.photo + '_'+ picObj.secret +'.jpg';
 		urlArray.push(picUrl);
 	});
 	console.log(urlArray);
